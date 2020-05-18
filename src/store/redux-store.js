4154d354086b47de21dbc6532b125e4fe69c3070
Adilet <adiletbeyshekeev@mail.ru>
2020-05-18 21:22:32 +0600

    delete redux dev tools

--- src/store/redux-store.js
+++ src/store/redux-store.js
@@ -5,10 +5,7 @@ import { virus_data } from "../store/virus_data";
 
 const reducers = combineReducers({ virus_data });
 
-export const redux_store = createStore(reducers, compose(
-    applyMiddleware(thunkMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
+export const redux_store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
 window.get_store = redux_store.getState();
 
